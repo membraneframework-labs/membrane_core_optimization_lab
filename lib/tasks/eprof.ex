199e7647ca40df1e3502c0cf6c1ab452167b859b
Jakub Balinski <balinski.jakub@gmail.com>
2021-08-10 14:31:27 +0200

    replace macros with regular calls to Mix.Tasks.Profile

--- lib/tasks/eprof.ex
+++ lib/tasks/eprof.ex
@@ -1,24 +1,13 @@
 defmodule Mix.Tasks.Eprof do
   use Mix.Task
 
-  defmacro profile(do: block) do
-    content =
-      quote do
-        Mix.Tasks.Profile.Eprof.profile(
-          fn -> unquote(block) end,
-          warmup: false,
-          callers: true,
-          sort: :time
-        )
-      end
-
-    Code.compile_quoted(content)
-  end
-
   @impl Mix.Task
   def run(_) do
-    profile do
-      OptimizationLab.Pipeline.run_for(10_000)
-    end
+    Mix.Tasks.Profile.Eprof.profile(
+      fn -> OptimizationLab.Pipeline.run_for(10_000) end,
+      warmup: false,
+      callers: true,
+      sort: :time
+    )
   end
 end
--- lib/tasks/fprof.ex
+++ lib/tasks/fprof.ex
@@ -1,24 +1,13 @@
 defmodule Mix.Tasks.Fprof do
   use Mix.Task
 
-  defmacro profile(do: block) do
-    content =
-      quote do
-        Mix.Tasks.Profile.Fprof.profile(
-          fn -> unquote(block) end,
-          warmup: false,
-          callers: true,
-          sort: :acc
-        )
-      end
-
-    Code.compile_quoted(content)
-  end
-
   @impl Mix.Task
   def run(_) do
-    profile do
-      OptimizationLab.Pipeline.run_for(10_000)
-    end
+    Mix.Tasks.Profile.Fprof.profile(
+      fn -> OptimizationLab.Pipeline.run_for(10_000) end,
+      warmup: false,
+      callers: true,
+      sort: :acc
+    )
   end
 end
